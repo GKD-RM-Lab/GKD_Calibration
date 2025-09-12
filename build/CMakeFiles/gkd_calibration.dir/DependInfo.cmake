2bc60faeecf38436bb8f5f3112f57bb277e6512f
FishSWA <fishswaa@gmail.com>
2025-09-12 16:17:00 +0800

    add

---
+++ build/CMakeFiles/gkd_calibration.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/gkd/gkd_calibrattion/calibration/cali_HIKdriver.cpp" "CMakeFiles/gkd_calibration.dir/calibration/cali_HIKdriver.cpp.o" "gcc" "CMakeFiles/gkd_calibration.dir/calibration/cali_HIKdriver.cpp.o.d"
+  "/home/gkd/gkd_calibrattion/calibration/calibrate_gkd.cpp" "CMakeFiles/gkd_calibration.dir/calibration/calibrate_gkd.cpp.o" "gcc" "CMakeFiles/gkd_calibration.dir/calibration/calibrate_gkd.cpp.o.d"
+  "/home/gkd/gkd_calibrattion/calibration/calibrate_gkd_main.cpp" "CMakeFiles/gkd_calibration.dir/calibration/calibrate_gkd_main.cpp.o" "gcc" "CMakeFiles/gkd_calibration.dir/calibration/calibrate_gkd_main.cpp.o.d"
+  "/home/gkd/gkd_calibrattion/calibration/calibrate_parameter_gkd.cpp" "CMakeFiles/gkd_calibration.dir/calibration/calibrate_parameter_gkd.cpp.o" "gcc" "CMakeFiles/gkd_calibration.dir/calibration/calibrate_parameter_gkd.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
